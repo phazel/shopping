7c85e8be34bd64139f044a7f2ba988f0c0744b3e
phazel <courtney.p.hazel@gmail.com>
2019-07-03 19:17:04 +1000

    Purchase a VGA adaptor

--- lib/checkout.rb
+++ lib/checkout.rb
@@ -1,5 +1,22 @@
+require 'pry'
+
 class Checkout
+
+  PRICES = {
+    vga: 30
+  }
+
+  def initialize()
+    @total = 0
+  end
+
+  def scan(sku)
+    if PRICES.key? sku
+      @total += PRICES[sku]
+    end
+  end
+
   def total
-    4
+    @total
   end
 end
--- spec/checkout_spec.rb
+++ spec/checkout_spec.rb
@@ -3,5 +3,8 @@ require 'checkout'
 describe Checkout do
   let(:co) { described_class.new }
 
-  it { expect(co.total).to eq 4 }
+  it 'can purchase a VGA adapter' do
+    result = co.scan(:vga)
+    expect(co.total).to eq 30
+  end
 end
