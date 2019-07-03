78f2dc6f0e8c9ba0c604bbecf64bb4a9c022226f
phazel <courtney.p.hazel@gmail.com>
2019-07-04 08:50:05 +1000

    Refactor BulkRule

--- lib/bulk_rule.rb
+++ lib/bulk_rule.rb
@@ -2,11 +2,21 @@ class BulkRule
   # for each bulk pricing rule, check if there are more than X many of that sku
   # if there are, replace all their prices.
   def self.apply(rule, scanned)
-    if scanned.count { |item| item[:sku] == rule[:sku] } >= rule[:minimum_activation_number]
-      scanned = scanned.map do |purchase|
-        purchase.merge({ price: rule[:new_price] })
-      end
+    if meets_minimum_number(rule, scanned)
+      scanned = replace_prices(rule, scanned)
     end
     scanned
+  end
+
+  private
+
+  def self.meets_minimum_number(rule, scanned)
+    scanned.count { |item| item[:sku] == rule[:sku] } >= rule[:minimum_activation_number]
+  end
+
+  def self.replace_prices(rule, scanned)
+    scanned.map do |purchase|
+      purchase.merge({ price: rule[:new_price] })
+    end
   end
 end
