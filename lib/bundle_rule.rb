04472e23a7c4b0be1e70c4b8beed6487cd94f43e
phazel <courtney.p.hazel@gmail.com>
2019-07-04 13:07:23 +1000

    Refactor Bundle Rule

--- lib/bundle_rule.rb
+++ lib/bundle_rule.rb
@@ -2,18 +2,23 @@ module Checkout
   module Rules
     class BundleRule
       def self.apply(rule, scanned)
-        # for however many MBPs there are, make that many VGAs free
-        number_of_buy_ones = scanned.count { |item| item[:sku] == rule[:buy_one] }
+        count_buy_ones = count_buy_ones(rule, scanned)
 
-        get_one_free_items = scanned.select { |item| item[:sku] == rule[:get_one_free] }
-
-        get_one_free_items.map.with_index do |item, i|
-          if i < number_of_buy_ones
+        get_one_free_items(rule, scanned).map.with_index do |item, i|
+          if i < count_buy_ones
             item[:price] = BigDecimal("0")
           end
         end
 
         scanned
+      end
+
+      def self.get_one_free_items(rule, scanned)
+        scanned.select { |item| item[:sku] == rule[:get_one_free] }
+      end
+
+      def self.count_buy_ones(rule, scanned)
+        scanned.count { |item| item[:sku] == rule[:buy_one] }
       end
     end
   end
