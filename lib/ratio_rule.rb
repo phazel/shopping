99dd258c25964160b90719db28b468c6cd08550e
phazel <courtney.p.hazel@gmail.com>
2019-07-04 11:40:06 +1000

    Refactor Ratio Rule

--- lib/ratio_rule.rb
+++ lib/ratio_rule.rb
@@ -2,12 +2,22 @@ module Checkout
   module Rules
     class RatioRule
       def self.apply(rule, scanned)
-        scanned.select { |item| item[:sku] == rule[:sku] }.map.with_index do |item, i|
-          if i % rule[:activation_number] == rule[:activation_number] - 1
+        relevant_items(rule, scanned).map.with_index do |item, i|
+          if item_needs_discount(rule, i)
             item[:price] = BigDecimal("0")
           end
         end
         scanned
+      end
+
+      private
+
+      def self.relevant_items(rule, scanned)
+        scanned.select { |item| item[:sku] == rule[:sku] }
+      end
+
+      def self.item_needs_discount(rule, i)
+        i % rule[:activation_number] == rule[:activation_number] - 1
       end
     end
   end
